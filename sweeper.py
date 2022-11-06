bbc80f3f4e683b06c471b53cb30962fb51efc2c1
Spencer <spencer@verhoff.net>
2022-11-06 14:11:33 -0500

    encapsulate tileset behavior

--- sweeper.py
+++ sweeper.py
@@ -1,143 +1,31 @@
 import pygame as pg
-import sys, random
-from tile import Tile
-from math import floor
+from tile import Tile, TileSet, TileImgs
+import sys
 
 pg.init()  # initialize pygame modules
+game_over = False
+tile_set = TileSet(20, 20, 75)
 
 
-# Return surface with requested size (resizes tiles for width/height)
-def get_tiles(size=(1, 1), img=None, width_px=None, height_px=None):
-    # if width_px is None or height_px is None:
-    #    screen = pg.display.set_mode(width_px, height_px)
-    tile = img
-    t_width = tile.get_width()
-    t_height = tile.get_height()
-    t_rect = tile.get_rect()
-    x, y = 0, 0
-    grid_w, grid_h = size
-
-    tile_grid = []
-
-    for th in range(grid_h):
-        tile_grid.append([])
-        for tw in range(grid_w):
-            tr = t_rect.move(x, y)
-            tile = Tile(tr)
-            tile_grid[th].append(tile)
-            x += t_width
-        x = 0
-        y += t_height
-    return tile_grid
 
 
-def draw_tiles(img, tile_grid):
-    width = len(tile_grid[0]) * img.get_width()
-    height = len(tile_grid) * img.get_height()
+
+def draw_tiles(img, tile_set):
+    width = tile_set.size_x * img.get_width()
+    height = tile_set.size_y * img.get_height()
     size = width, height
     screen = pg.display.set_mode(size)
-    for t_list in tile_grid:
+    for t_list in tile_set.grid:
         for tile in t_list:
             screen.blit(img, tile.rect)
 
     return screen
 
-
-def get_tile_clicked(pos):
-    x, y = pos
-    surf = pg.display.get_surface()
-    s_width = surf.get_width()
-    s_height = surf.get_height()
-    t_width, t_height = tile_size
-    xt = floor(x / (s_width / t_width))
-    yt = floor(y / (s_height / t_height))
-    # print(f"Screen: {s_width}, {s_height}")
-    # print(f"Tiles: {t_width}, {t_height}")
-    # print(f"Click: {x}, {y}")
-    # print(f"Return: {xt}, {yt}")
-    return tiles[yt][xt]
-
-# Get the tile at position clicked
-#def get_tile(pos):
-#    x, y = pos
-#    xt, yt = get_tile_clicked(tile_size, x, y)
-#s    return t
+screen = draw_tiles(TileImgs.reg, tile_set)
 
 
-def increment_mines(add_list):
-    for tile in add_list:
-        try:
-            tile.num_mines += 1
-        except:
-            pass
-
-def set_mines(tile_set, num_mines):
-    tiles_flat = [t for tl in tile_set for t in tl]
-    random.shuffle(tiles_flat)
-    for t in tiles_flat[:num_mines]:
-        t.is_mine = True
-
-    index_mods = [
-                (-1, -1),
-                (-1, 0),
-                (-1, 1),
-                (0, -1),
-                (0, 1),
-                (1, -1),
-                (1, 0),
-                (1, 1)
-            ]
-
-    
-    for tl_i, t_list in enumerate(tile_set):
-        for t_i, t in enumerate(t_list):
-            if not t.is_mine:
-                continue
-
-            increment = []
-            for x, y in index_mods:
-                if t_i + x < 0 or t_i + x >= len(t_list):
-                    continue
-                if tl_i + y < 0 or tl_i + y >= len(tile_set):
-                    continue
-                increment.append((x, y))
-
-            for mx, my in increment:
-                #print(t, mx, my)
-                tile_set[tl_i + my][t_i + mx].num_mines += 1
-
-            '''    
-            add_list = (
-                tile_set[tl_i - 1][t_i - 1 : t_i + 2]
-                + t_list[t_i - 1 : t_i + 2 : 2]
-                + tile_set[tl_i + 1][t_i - 1 : t_i + 2]
-            )
-            increment_mines(add_list)
-            '''
-    
-
-game_over = False
-
-# size = width, height = 1080, 720
-tile_img = pg.image.load("imgs/tile.png")
-tile_mine = pg.image.load("imgs/tile_mine.png")
-tile_flag = pg.image.load("imgs/tile_flag.png")
-
-tile_nums = {}
-
-for i in range(9):
-    tile_nums[i] = pg.image.load(f"imgs/tile_{i}.png")
-
-
-tile_size = 18, 20
-tiles = get_tiles(tile_size, tile_img)
-screen = draw_tiles(tile_img, tiles)
-
-num_mines = 75
-set_mines(tiles, num_mines)
 
 while 1:
-
     for event in pg.event.get():
         if event.type == pg.QUIT:
             sys.exit()
@@ -147,23 +35,29 @@ while 1:
 
         if event.type == pg.MOUSEBUTTONUP:
             pos = pg.mouse.get_pos()
-            t = get_tile_clicked(pos)
+            t = tile_set.get_tile_clicked(pos, pg.display.get_surface())
             if event.button == 1 and not t.flagged:    
                 t.clicked = True
                 print(t)
                 if t.is_mine:
-                    screen.blit(tile_mine, t)
+                    for m in tile_set.mines:
+                        screen.blit(TileImgs.mine, m)
+                    screen.blit(TileImgs.mine_click, t)
                     game_over = True
                 else:
                     # if 0, get_tile_clicked recursively for surrounding tiles
-                    screen.blit(tile_nums[t.num_mines], t)
+                    if t.num_mines == 0:
+                        pass
+                    screen.blit(TileImgs.nums[t.num_mines], t)
+
                     
             if event.button == 3 and not t.clicked:
                 if t.flagged:
                     t.flagged = False
+                    screen.blit(TileImgs.reg, t)
                 else:
                     t.flagged = True
-                screen.blit(tile_flag, t)
+                    screen.blit(TileImgs.flag, t)
 
 
     pg.display.flip()
--- tile.py
+++ tile.py
@@ -1,3 +1,16 @@
+import pygame as pg
+import sys, random
+from math import floor
+
+class TileImgs():
+    reg = pg.image.load("imgs/tile.png")
+    mine = pg.image.load("imgs/tile_mine.png")
+    flag = pg.image.load("imgs/tile_flag.png")
+    mine_click = pg.image.load("imgs/tile_mine_click.png")
+    nums = {}
+
+    for i in range(9):
+        nums[i] = pg.image.load(f"imgs/tile_{i}.png")
 
 
 class Tile:
@@ -12,3 +25,98 @@ class Tile:
     def __str__(self):
         r = f'Clicked: {self.clicked} Mine: {self.is_mine} Num Mines: {self.num_mines} Flagged: {self.flagged}'
         return r
+
+
+class TileSet():
+
+    index_mods = [
+        (-1, -1),
+        (-1, 0),
+        (-1, 1),
+        (0, -1),
+        (0, 1),
+        (1, -1),
+        (1, 0),
+        (1, 1)
+    ]
+
+    def __init__(self, size_x, size_y, num_mines):
+        self.size_x = size_x
+        self.size_y = size_y
+        self.grid = self.get_tiles(size_x, size_y, TileImgs.reg)
+        self.num_mines = num_mines
+        self.mines = self.set_mines(num_mines)
+
+
+    # Return surface with requested size (needs to resize tiles for width/height)
+    def get_tiles(self, size_x, size_y, img=None, width_px=None, height_px=None):
+        # if width_px is None or height_px is None:
+        #    self.screen = pg.display.set_mode(width_px, height_px)
+        tile = img
+        print(tile)
+        t_width = tile.get_width()
+        t_height = tile.get_height()
+        t_rect = tile.get_rect()
+        x, y = 0, 0
+
+        grid = []
+
+        for th in range(size_y):
+            grid.append([])
+            for tw in range(size_x):
+                tr = t_rect.move(x, y)
+                tile = Tile(tr)
+                grid[th].append(tile)
+                x += t_width
+            x = 0
+            y += t_height
+        return grid
+
+
+    # Returns the tile at the position
+    def get_tile_clicked(self, pos, surface):
+        x, y = pos
+        s_width = surface.get_width()
+        s_height = surface.get_height()
+        t_width, t_height = self.size_x, self.size_y
+        xt = floor(x / (s_width / t_width))
+        yt = floor(y / (s_height / t_height))
+        return self.grid[yt][xt]
+
+
+    # Returns the valid indexes surrounding tile
+    def get_tiles_near(self, tile_list_i, tile_i):
+        tl_i = tile_list_i
+        t_i = tile_i
+        indexes = []
+        for x, y in TileSet.index_mods:
+            if t_i + x < 0 or t_i + x >= len(self.grid[0]):
+                continue
+            if tl_i + y < 0 or tl_i + y >= len(self.grid):
+                continue
+            indexes.append((t_i+x, tl_i+y))
+        return indexes
+
+
+    # Set the minefield and precalculate all tile numbers
+    def set_mines(self, num_mines):
+        tiles_flat = [t for tl in self.grid for t in tl]
+        random.shuffle(tiles_flat)
+        self.mines = tiles_flat[:num_mines]
+
+        for t in self.mines:
+            t.is_mine = True
+
+        for tl_i, t_list in enumerate(self.grid):
+            for t_i, t in enumerate(t_list):
+                if not t.is_mine:
+                    continue
+
+                valid_indexes = self.get_tiles_near(tl_i, t_i)
+                for x, y in valid_indexes:
+                    #print(t, mx, my)
+                    self.grid[y][x].num_mines += 1
+
+        return self.mines    
+
+
